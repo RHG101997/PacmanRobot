d875d90340eafc78bc117b3dbf04c5649d5f2f1c
rhg101997 <rhg101997@gmail.com>
2020-10-19 16:12:10 -0400

    added manual control (tetsing)

---
+++ movement/controller.py
@@ -0,0 +1,27 @@
+import keyboard
+import socket
+import os
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "10.0.0.142"
+port = 1235
+
+s.connect((host,port))
+
+while True:  # making a loop
+    try:  
+        if(keyboard.is_pressed('q')):  # if key 'q' is pressed 
+            s.send(bytes("q","utf-8"))
+        elif (keyboard.is_pressed('w')):
+            s.send(bytes("w","utf-8"))
+        elif (keyboard.is_pressed('s')):
+            s.send(bytes("s","utf-8"))
+        elif (keyboard.is_pressed('a')):
+            s.send(bytes("a","utf-8"))
+        elif (keyboard.is_pressed('d')):
+            s.send(bytes("d","utf-8"))
+        else:
+            s.send(bytes('stop',"utf-8"))      
+    except:
+        break  
+s.close()
---
+++ reciever.py
@@ -0,0 +1,51 @@
+import socket
+import threading
+import os
+from move import Move
+
+
+def action(msg,move):
+    if(msg == "w"):
+        print("Foward")
+        move.forward()
+    elif (msg == "s"):
+        print("Reverse")
+        move.reverse()
+    elif (msg == "a"):
+        print("Left")
+        move.turnLeft()
+    elif (msg == "d"):
+        print("Right")
+        move.turnRight()
+    else:
+        print("Stopped")
+        move.stop()
+     
+        
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+hostname = socket.gethostname()
+host = socket.gethostbyname(hostname)
+port  = 1235
+move = Move(config["Move"])
+
+
+
+s.bind((host, port))
+print("Waiting")
+s.listen(3)
+notDone = True
+
+while notDone:
+    conn, addr = s.accept()
+    print("Connected")
+    while True:
+        msg = conn.recv(1024)
+        if(msg.decode("utf-8") == "q"):
+            notDone = False
+            break
+        else:
+            action(msg.decode("utf-8"),move)
+
+conn.close()
+s.close()
