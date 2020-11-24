1855463c4dedb75cb8e59652af5662fc45834054
rhg101997 <rhg101997@gmail.com>
2020-11-24 11:45:21 -0500

    new

--- test.py
+++ test.py
@@ -8,9 +8,11 @@ config.read("config.ini")
 move = Move(config["Move"])
 
 # 1400  arounf 90 degree
+print("forward")
 move.moveBy(1000)
+print("right")
 move.turnByRight(1400)
-
+print("done")
 
 
 
