489afc3ca5a18e910d282a939359d8d647e83346
rhg101997 <rhg101997@gmail.com>
2020-11-24 17:00:44 -0500

    new

--- move.py
+++ move.py
@@ -119,7 +119,7 @@ class Move:
         self.encRight.reset()
         self.forward()
         while(self.encRight.read() < steps and self.encLeft.read() < steps):
-            self.changeSpeed(self.calSpeed(self.speed ,steps))
+            # self.changeSpeed(self.calSpeed(self.speed ,steps))
             time.sleep(0.01)
         self.stop()
     
--- test.py
+++ test.py
@@ -24,6 +24,6 @@ move = Move(config["Move"])
 
 
 move.moveBy(3165) # feet
-move.turnByRight(1495)
+move.turnByRight(1495) # 1495
 move.moveBy(4885) # feet
 
