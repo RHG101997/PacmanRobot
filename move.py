ed05c9a3fbf790da71809e923a51c55f31ecef93
rhg101997 <rhg101997@gmail.com>
2020-11-24 00:49:24 -0500

    bug on moveBy

--- move.py
+++ move.py
@@ -116,9 +116,11 @@ class Move:
 
     # Added encoders to Motors
     def moveBy(self,steps):
-        while(steps >= self.enc1.read()):
-            self.forward()
-        self.stop()
+        self.forward()
+        while(True):
+            if(self.enc1.read() > 2552):
+                self.stop()
+                break
 
 
     def calibrateRobot(self):
