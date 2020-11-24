9d861b484117ce28e55b4c23d3013532a00d2f7e
rhg101997 <rhg101997@gmail.com>
2020-11-24 10:27:34 -0500

    testing

--- movement/encoder_class.py
+++ movement/encoder_class.py
@@ -2,4 +2,4 @@ import Encoder
 
 enc = Encoder.Encoder(21,20)
 enc2 = Encoder.Encoder(5,6)
-print("left: " + int(enc.read()) + "right: " + int(enc2.read()))
+print("left: " + enc.read() + "right: " + enc2.read())
