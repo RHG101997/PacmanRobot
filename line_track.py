7ded60da0946c9a30d48acacc19c8007bd89cf3e
rhg101997 <rhg101997@gmail.com>
2020-11-28 12:59:25 -0500

    atrack

--- line_track.py
+++ line_track.py
@@ -23,13 +23,13 @@ gpio.setup(R, gpio.IN)
 while True:
     time.sleep(0.5)
     if(not gpio.input(M)):
-        move.reverse()
+        # move.reverse()
         print('forward')
     elif (not gpio.input(R)):
-        move.turnRight()
+        move.turnRightByTime(0.1)
         print('right')
     elif (not gpio.input(L)):
-        # move.turnLeft()
+        move.turnLeftByTime(0.1)
         print('left')
     elif (not(gpio.input(M) and gpio.input(R) and gpio.input(L))):
         # move.stop()
